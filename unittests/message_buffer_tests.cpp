310ab5a5a351c5bb157d2d1cb0baf16cd1d66b94
tianyalangzi <caiwei1203@sina.cn>
2019-03-04 21:22:07 +0800

    Update message_buffer_tests.cpp

--- unittests/message_buffer_tests.cpp
+++ unittests/message_buffer_tests.cpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 
 #include <fc/network/message_buffer.hpp>
