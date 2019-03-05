e8cf44d5d22ab76ef3b2549ae7425eda7d582f8b
tianyalangzi <caiwei1203@sina.cn>
2019-03-05 15:59:26 +0800

    Update login_plugin.cpp

--- plugins/login_plugin/login_plugin.cpp
+++ plugins/login_plugin/login_plugin.cpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #include <eosio/chain/authorization_manager.hpp>
 #include <eosio/chain/exceptions.hpp>
