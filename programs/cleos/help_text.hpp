2511292d133c2b1d538dee3965759589a30e9724
tianyalangzi <caiwei1203@sina.cn>
2019-03-05 11:24:28 +0800

    Update help_text.hpp

--- programs/cleos/help_text.hpp
+++ programs/cleos/help_text.hpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #pragma once
 #include <fc/exception/exception.hpp>
