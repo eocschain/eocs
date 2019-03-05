b32c02d612eb3b71ad39aee727d043161f77785f
Breeze <secondtonone1@163.com>
2019-03-05 17:44:56 +0800

    Update abi_serializer.cpp

--- libraries/chain/abi_serializer.cpp
+++ libraries/chain/abi_serializer.cpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #include <eosio/chain/abi_serializer.hpp>
 #include <eosio/chain/contract_types.hpp>
