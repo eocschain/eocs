adf6454a867d30da9439f89ed8b76e4452c81ffe
Breeze <secondtonone1@163.com>
2019-03-05 16:47:53 +0800

    Update protocol.hpp

--- libraries/chain/include/eosio/chain/protocol.hpp
+++ libraries/chain/include/eosio/chain/protocol.hpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #pragma once
 #include <eosio/chain/block.hpp>
