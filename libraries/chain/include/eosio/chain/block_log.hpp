28dd4e25de5291d0dc17743d478ccd863a41ed73
Breeze <secondtonone1@163.com>
2019-03-05 15:25:59 +0800

    Update block_log.hpp

--- libraries/chain/include/eosio/chain/block_log.hpp
+++ libraries/chain/include/eosio/chain/block_log.hpp
@@ -1,10 +1,9 @@
 /**
  *  @file
 <<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+ 
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #pragma once
 #include <fc/filesystem.hpp>
