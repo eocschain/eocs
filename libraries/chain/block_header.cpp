1bd42663b3a44c2e365710bb8293de6799d66303
Breeze <secondtonone1@163.com>
2019-03-05 18:01:26 +0800

    Update block_header.cpp

--- libraries/chain/block_header.cpp
+++ libraries/chain/block_header.cpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #include <eosio/chain/block.hpp>
 #include <eosio/chain/merkle.hpp>
