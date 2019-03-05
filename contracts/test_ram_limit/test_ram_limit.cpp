d1e0245031ad5158143e761db9d0cf2232407378
Breeze <secondtonone1@163.com>
2019-03-05 14:39:59 +0800

    Update test_ram_limit.cpp

--- contracts/test_ram_limit/test_ram_limit.cpp
+++ contracts/test_ram_limit/test_ram_limit.cpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #include <utility>
 #include <vector>
