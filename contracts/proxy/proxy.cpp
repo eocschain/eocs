347a417b7277aa47ce13b99a992e212468fe0f31
Breeze <secondtonone1@163.com>
2019-03-05 14:19:51 +0800

    Update proxy.cpp

--- contracts/proxy/proxy.cpp
+++ contracts/proxy/proxy.cpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #include <proxy/proxy.hpp>
 #include <eosiolib/transaction.hpp>
