82d7e270fe0cdfad90d7cf03b13186db9733dca2
tianyalangzi <caiwei1203@sina.cn>
2019-03-05 11:51:17 +0800

    Update test_control_api_plugin.hpp

--- plugins/test_control_api_plugin/include/eosio/test_control_api_plugin/test_control_api_plugin.hpp
+++ plugins/test_control_api_plugin/include/eosio/test_control_api_plugin/test_control_api_plugin.hpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #pragma once
 #include <eosio/test_control_plugin/test_control_plugin.hpp>
