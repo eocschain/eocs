f569984d61617827f71ad4db61e5c91dfb5a7a90
Breeze <secondtonone1@163.com>
2019-03-05 10:11:47 +0800

    Update eosio.bios.cpp

--- contracts/eosio.bios/eosio.bios.cpp
+++ contracts/eosio.bios/eosio.bios.cpp
@@ -1,7 +1,4 @@
 #include <eosio.bios/eosio.bios.hpp>
 
-<<<<<<< HEAD
 EOSIO_ABI( eosio::bios, (setpriv)(setalimits)(setglimits)(setprods)(setparams)(reqauth)(setsymbol) )
-=======
-EOSIO_ABI( eosio::bios, (setpriv)(setalimits)(setglimits)(setprods)(setparams)(reqauth) )
->>>>>>> otherb
+
