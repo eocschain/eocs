c3a6bf8e2782699fa088e54ef4b2ff0e84ec66b4
Breeze <secondtonone1@163.com>
2019-03-05 15:43:30 +0800

    Update chain_id_type.hpp

--- libraries/chain/include/eosio/chain/chain_id_type.hpp
+++ libraries/chain/include/eosio/chain/chain_id_type.hpp
@@ -1,10 +1,8 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #pragma once
 
@@ -12,7 +10,7 @@
 
 struct hello;
 
-<<<<<<< HEAD
+
 namespace eoc_icp{
   class relay;
   struct hello;
@@ -22,18 +20,16 @@ namespace eoc_icp{
   struct icp_handshake_message;
 }
 
-=======
->>>>>>> otherb
+
 namespace eosio {
 
    class net_plugin_impl;
    struct handshake_message;
-<<<<<<< HEAD
+
    struct difchain_message;
    struct connection_impl;
    
-=======
->>>>>>> otherb
+
 
    namespace chain_apis {
       class read_only;
@@ -44,11 +40,10 @@ namespace chain {
    struct chain_id_type : public fc::sha256 {
       using fc::sha256::sha256;
 
-<<<<<<< HEAD
+
       chain_id_type() = default;
 
-=======
->>>>>>> otherb
+
       template<typename T>
       inline friend T& operator<<( T& ds, const chain_id_type& cid ) {
         ds.write( cid.data(), cid.data_size() );
@@ -61,17 +56,13 @@ namespace chain {
         return ds;
       }
 
-<<<<<<< HEAD
-      void reflector_verify()const;
 
-      private:
-=======
       void reflector_init()const;
 
       private:
          chain_id_type() = default;
 
->>>>>>> otherb
+
          // Some exceptions are unfortunately necessary:
          template<typename T>
          friend T fc::variant::as()const;
@@ -80,7 +71,6 @@ namespace chain {
 
          friend class eosio::net_plugin_impl;
          friend struct eosio::handshake_message;
-<<<<<<< HEAD
          friend struct eosio::difchain_message;
          friend class connection;
          friend class eoc_icp::relay;
@@ -90,10 +80,7 @@ namespace chain {
          friend struct eoc_icp::get_info_results;
          friend class eoc_icp::icp_connection;
          friend struct eoc_icp::icp_handshake_message;
-=======
 
-         friend struct ::hello; // TODO: Rushed hack to support bnet_plugin. Need a better solution.
->>>>>>> otherb
    };
 
 } }  // namespace eosio::chain
