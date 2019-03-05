7dbbdc5aa58918c399a1b7f9be875bf50b2123e0
tianyalangzi <caiwei1203@sina.cn>
2019-03-05 18:01:25 +0800

    Update wast_to_wasm.cpp

--- libraries/chain/wast_to_wasm.cpp
+++ libraries/chain/wast_to_wasm.cpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #include <eosio/chain/wast_to_wasm.hpp>
 #include <Inline/BasicTypes.h>
