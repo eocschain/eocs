d10ee5e96fa095db802c89c9935f2d09cb10e0f5
Breeze <secondtonone1@163.com>
2019-03-05 14:31:42 +0800

    Update test_compiler_builtins.cpp

--- contracts/test_api/test_compiler_builtins.cpp
+++ contracts/test_api/test_compiler_builtins.cpp
@@ -1,10 +1,8 @@
 /**
  *    @file test_compiler_builtins.cpp
-<<<<<<< HEAD
- *    @copyright defined in eos/LICENSE.txt
-=======
+
  *    @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 
 #include <eosiolib/eosio.hpp>
