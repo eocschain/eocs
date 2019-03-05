91de664a6bd02e78c5d1dc4e3f8da643bb06b96b
Breeze <secondtonone1@163.com>
2019-03-05 16:55:16 +0800

    Update reversible_block_object.hpp

--- libraries/chain/include/eosio/chain/reversible_block_object.hpp
+++ libraries/chain/include/eosio/chain/reversible_block_object.hpp
@@ -1,11 +1,9 @@
 
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
+
  */
 #pragma once
 #include <eosio/chain/types.hpp>
