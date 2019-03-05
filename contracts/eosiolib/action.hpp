6186be13d716e404050b5d5e3547e49b5e10a1ba
Breeze <secondtonone1@163.com>
2019-03-05 11:36:33 +0800

    Update action.hpp

--- contracts/eosiolib/action.hpp
+++ contracts/eosiolib/action.hpp
@@ -1,10 +1,7 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
+
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #pragma once
 #include <eosiolib/action.h>
