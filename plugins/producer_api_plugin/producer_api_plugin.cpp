bb1181a3c6b82d38c0219f4e745bf5ad2bd08a52
tianyalangzi <caiwei1203@sina.cn>
2019-03-05 14:30:08 +0800

    Update producer_api_plugin.cpp

--- plugins/producer_api_plugin/producer_api_plugin.cpp
+++ plugins/producer_api_plugin/producer_api_plugin.cpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #include <eosio/producer_api_plugin/producer_api_plugin.hpp>
 #include <eosio/chain/exceptions.hpp>
