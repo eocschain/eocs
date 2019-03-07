177d5b613163056960ea5b2963efe9dc69d343f5
tianyalangzi <caiwei1203@sina.cn>
2019-03-07 11:53:18 +0800

    Update special_accounts_tests.cpp

--- unittests/special_accounts_tests.cpp
+++ unittests/special_accounts_tests.cpp
@@ -1,10 +1,6 @@
 /**
  *  @file
-<<<<<<< HEAD
- *  @copyright defined in eos/LICENSE.txt
-=======
  *  @copyright defined in eos/LICENSE
->>>>>>> otherb
  */
 #include <algorithm>
 #include <vector>
