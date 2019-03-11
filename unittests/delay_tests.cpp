694f4593baa73b4ff0b0b2e7ce069907c8fae705
tianyalangzi <caiwei1203@sina.cn>
2019-03-11 21:01:45 +0800

    Update delay_tests.cpp

--- unittests/delay_tests.cpp
+++ unittests/delay_tests.cpp
@@ -78,13 +78,8 @@ BOOST_FIXTURE_TEST_CASE( delay_error_create_account, validating_tester) { try {
    produce_blocks(6);
 
    auto scheduled_trxs = get_scheduled_transactions();
-<<<<<<< HEAD
-
-   BOOST_REQUIRE_EQUAL(scheduled_trxs.size(), 1);
-=======
    BOOST_REQUIRE_EQUAL(scheduled_trxs.size(), 1u);
 
->>>>>>> eosiobranch
    auto dtrace = control->push_scheduled_transaction(scheduled_trxs.front(), fc::time_point::maximum());
    BOOST_REQUIRE_EQUAL(dtrace->except.valid(), true);
    BOOST_REQUIRE_EQUAL(dtrace->except->code(), missing_auth_exception::code_value);
