32e203d8440a9d46f9472b741ddcf53a43208fe5
tianyalangzi <caiwei1203@sina.cn>
2019-03-11 21:17:17 +0800

    Update eosio_build_amazon.sh

--- scripts/eosio_build_amazon.sh
+++ scripts/eosio_build_amazon.sh
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-	OS_VER=$( grep VERSION_ID /etc/os-release | cut -d'=' -f2 | sed 's/[^0-9\.]//gI' | cut -d'.' -f1 )
-
-	MEM_MEG=$( free -m | sed -n 2p | tr -s ' ' | cut -d\  -f2 )
-	CPU_SPEED=$( lscpu | grep "MHz" | tr -s ' ' | cut -d\  -f3 | cut -d'.' -f1 )
-	CPU_CORE=$( lscpu -pCPU | grep -v "#" | wc -l )
-	MEM_GIG=$(( ((MEM_MEG / 1000) / 2) ))
-	JOBS=$(( MEM_GIG > CPU_CORE ? CPU_CORE : MEM_GIG ))
-
-	DISK_TOTAL=$( df -h . | grep /dev | tr -s ' ' | cut -d\  -f2 | sed 's/[^0-9]//' )
-	DISK_AVAIL=$( df -h . | grep /dev | tr -s ' ' | cut -d\  -f4 | sed 's/[^0-9]//' )
-
-	printf "\\n\\tOS name: %s\\n" "${OS_NAME}"
-	printf "\\tOS Version: %s\\n" "${OS_VER}"
-	printf "\\tCPU speed: %sMhz\\n" "${CPU_SPEED}"
-	printf "\\tCPU cores: %s\\n" "${CPU_CORE}"
-	printf "\\tPhysical Memory: %sMgb\\n" "${MEM_MEG}"
-	printf "\\tDisk space total: %sGb\\n" "${DISK_TOTAL}"
-	printf "\\tDisk space available: %sG\\n" "${DISK_AVAIL}"
-
-	if [ "${MEM_MEG}" -lt 7000 ]; then
-		printf "\\tYour system must have 7 or more Gigabytes of physical memory installed.\\n"
-		printf "\\texiting now.\\n"
-		exit 1
-	fi
-
-	if [[ "${OS_NAME}" == "Amazon Linux AMI" && "${OS_VER}" -lt 2017 ]]; then
-		printf "\\tYou must be running Amazon Linux 2017.09 or higher to install EOSIO.\\n"
-		printf "\\texiting now.\\n"
-		exit 1
-	fi
-
-	if [ "${DISK_AVAIL}" -lt "${DISK_MIN}" ]; then
-		printf "\\tYou must have at least %sGB of available storage to install EOSIO.\\n" "${DISK_MIN}"
-		printf "\\texiting now.\\n"
-		exit 1
-	fi
-
-	printf "\\n\\tChecking Yum installation.\\n"
-	if ! YUM=$( command -v yum 2>/dev/null )
-	then
-		printf "\\n\\tYum must be installed to compile EOS.IO.\\n"
-		printf "\\n\\tExiting now.\\n"
-		exit 1
-	fi
-	
-	printf "\\tYum installation found at %s.\\n" "${YUM}"
-	printf "\\tUpdating YUM.\\n"
-	if ! UPDATE=$( sudo "$YUM" -y update )
-	then
-		printf "\\n\\tYUM update failed.\\n"
-		printf "\\n\\tExiting now.\\n"
-		exit 1
-	fi
-	printf "\\t%s\\n" "${UPDATE}"
-
-	if [[ "${OS_NAME}" == "Amazon Linux AMI" ]]; then
-		DEP_ARRAY=( git gcc72.x86_64 gcc72-c++.x86_64 autoconf automake libtool make bzip2 \
-		bzip2-devel.x86_64 openssl-devel.x86_64 gmp-devel.x86_64 libstdc++72.x86_64 \
-		python27.x86_64 python27-devel.x86_64 python36-devel.x86_64 libedit-devel.x86_64 doxygen.x86_64 graphviz.x86_64)
-	else
-		DEP_ARRAY=( git gcc gcc-c++ autoconf automake libtool make bzip2 \
-		bzip2-devel openssl-devel gmp-devel libstdc++ \
-		python3 python3-devel python-devel libedit-devel doxygen graphviz)
-	fi
-	COUNT=1
-	DISPLAY=""
-	DEP=""
-
-	printf "\\n\\tChecking YUM for installed dependencies.\\n\\n"
-=======
 if [ $1 == 1 ]; then ANSWER=1; else ANSWER=0; fi
 
 OS_VER=$( grep VERSION_ID /etc/os-release | cut -d'=' -f2 | sed 's/[^0-9\.]//gI' | cut -d'.' -f1 )
@@ -92,7 +21,6 @@ else
 		python3 python3-devel python-devel libedit-devel doxygen graphviz 
 	)
 fi
->>>>>>> eosiobranch
 
 COUNT=1
 DISPLAY=""
@@ -104,39 +32,11 @@ if [[ "${OS_NAME}" == "Amazon Linux AMI" && "${OS_VER}" -lt 2017 ]]; then
 	exit 1
 fi
 
-<<<<<<< HEAD
-	if [ "${COUNT}" -gt 1 ]; then
-		printf "\\n\\tThe following dependencies are required to install EOSIO.\\n"
-		printf "\\n\\t${DISPLAY}\\n\\n"
-		printf "\\tDo you wish to install these dependencies?\\n"
-		if is_noninteractive; then exec <<< "1"; fi
-		select yn in "Yes" "No"; do
-			case $yn in
-				[Yy]* ) 
-					printf "\\n\\n\\tInstalling dependencies.\\n\\n"
-					if ! sudo "${YUM}" -y install ${DEP}
-					then
-						printf "\\n\\tYUM dependency installation failed.\\n"
-						printf "\\n\\tExiting now.\\n"
-						exit 1
-					else
-						printf "\\n\\tYUM dependencies installed successfully.\\n"
-					fi
-				break;;
-				[Nn]* ) printf "\\nUser aborting installation of required dependencies,\\n Exiting now.\\n"; exit;;
-				* ) echo "Please type 1 for yes or 2 for no.";;
-			esac
-		done
-	else 
-		printf "\\n\\tNo required YUM dependencies to install.\\n"
-	fi
-=======
 if [ "${DISK_AVAIL}" -lt "${DISK_MIN}" ]; then
 	printf "You must have at least %sGB of available storage to install EOSIO.\\n" "${DISK_MIN}"
 	printf "exiting now.\\n"
 	exit 1
 fi
->>>>>>> eosiobranch
 
 printf "\\nChecking Yum installation.\\n"
 if ! YUM=$( command -v yum 2>/dev/null )
