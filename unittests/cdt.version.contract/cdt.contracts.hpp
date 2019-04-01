5a61873bba0b0cc83ff968c0a0d81644243d0760
secondtonone <secondtonone1@163.com>
2019-04-01 11:07:27 +0800

    修改：     plugins/CMakeLists.txt 	修改：     plugins/eoc_relay_plugin/transaction.cpp 	修改：     plugins/icp_relay_plugin/transaction.cpp 	修改：     programs/nodeos/CMakeLists.txt 	修改：     unittests/cdt.version.contract/cdt.contracts.hpp

--- unittests/cdt.version.contract/cdt.contracts.hpp
+++ unittests/cdt.version.contract/cdt.contracts.hpp
@@ -4,16 +4,16 @@
 namespace eosio { namespace testing {
 
 struct contracts {
-   static std::vector<uint8_t> system_wasm() { return read_wasm("/data/eosco/eocs/unittests/cdt.version.contract/eosio.system/eosio.system.wasm"); }
-   static std::vector<char>    system_abi() { return read_abi("/data/eosco/eocs/unittests/cdt.version.contract/eosio.system/eosio.system.abi"); }
-   static std::vector<uint8_t> token_wasm() { return read_wasm("/data/eosco/eocs/unittests/cdt.version.contract/eosio.token/eosio.token.wasm"); }
-   static std::vector<char>    token_abi() { return read_abi("/data/eosco/eocs/unittests/cdt.version.contract/eosio.token/eosio.token.abi"); }
-   static std::vector<uint8_t> msig_wasm() { return read_wasm("/data/eosco/eocs/unittests/cdt.version.contract/eosio.msig/eosio.msig.wasm"); }
-   static std::vector<char>    msig_abi() { return read_abi("/data/eosco/eocs/unittests/cdt.version.contract/eosio.msig/eosio.msig.abi"); }
-   static std::vector<uint8_t> wrap_wasm() { return read_wasm("/data/eosco/eocs/unittests/cdt.version.contract/eosio.wrap/eosio.wrap.wasm"); }
-   static std::vector<char>    wrap_abi() { return read_abi("/data/eosco/eocs/unittests/cdt.version.contract/eosio.wrap/eosio.wrap.abi"); }
-   static std::vector<uint8_t> bios_wasm() { return read_wasm("/data/eosco/eocs/unittests/cdt.version.contract/eosio.bios/eosio.bios.wasm"); }
-   static std::vector<char>    bios_abi() { return read_abi("/data/eosco/eocs/unittests/cdt.version.contract/eosio.bios/eosio.bios.abi"); }
+   static std::vector<uint8_t> system_wasm() { return read_wasm("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.system/eosio.system.wasm"); }
+   static std::vector<char>    system_abi() { return read_abi("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.system/eosio.system.abi"); }
+   static std::vector<uint8_t> token_wasm() { return read_wasm("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.token/eosio.token.wasm"); }
+   static std::vector<char>    token_abi() { return read_abi("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.token/eosio.token.abi"); }
+   static std::vector<uint8_t> msig_wasm() { return read_wasm("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.msig/eosio.msig.wasm"); }
+   static std::vector<char>    msig_abi() { return read_abi("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.msig/eosio.msig.abi"); }
+   static std::vector<uint8_t> wrap_wasm() { return read_wasm("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.wrap/eosio.wrap.wasm"); }
+   static std::vector<char>    wrap_abi() { return read_abi("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.wrap/eosio.wrap.abi"); }
+   static std::vector<uint8_t> bios_wasm() { return read_wasm("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.bios/eosio.bios.wasm"); }
+   static std::vector<char>    bios_abi() { return read_abi("/home/secondtonone/workspace/eocs/unittests/cdt.version.contract/eosio.bios/eosio.bios.abi"); }
 
 };
 }} //ns eosio::testing
